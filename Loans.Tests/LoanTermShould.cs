0ec7c0d3b99fa4a4c8537711c314560d102031f8
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2019-08-31 15:19:57 +0530

    Ignore, lifeevent hook

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 
 namespace Loans.Tests
 {
-    [TestFixture]
     public class LoanTermShould
     {
         [Test]
--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -1,25 +1,55 @@
 ﻿using Loans.Domain.Applications;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Loans.Tests
 {
+    [Category("Product Comparison")]
     public class ProductComparerShould
     {
-        [Test]
-        public void ReturnCorrectNumberOfComparisons()
+        private List<LoanProduct> products;
+        private ProductComparer sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
-            var products = new List<LoanProduct>
+            // Simulate long setup init time for this list of products
+            // We assume that this list will not be modified by any tests
+            // as this will potentially break other tests (i.e. break test isolation)
+            products = new List<LoanProduct>
             {
                 new LoanProduct(1, "a", 1),
                 new LoanProduct(2, "b", 2),
                 new LoanProduct(3, "c", 3)
             };
+        }
 
-            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
 
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // Run after last test in this test class (fixture) executes
+            // e.g. disposing of shared expensive setup performed in OneTimeSetUp
+
+            // products.Dispose(); e.g. if products implemented IDisposable
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Runs after each test executes
+            // sut.Dispose();
+        }
+
+        [Test]        
+        public void ReturnCorrectNumberOfComparisons()
+        {
             List<MonthlyRepaymentComparison> comparisons =
                 sut.CompareMonthlyRepayments(new LoanTerm(30));
 
@@ -29,15 +59,6 @@ namespace Loans.Tests
         [Test]
         public void NotReturnDuplicateComparisons()
         {
-            var products = new List<LoanProduct>
-            {
-                new LoanProduct(1, "a", 1),
-                new LoanProduct(2, "b", 2),
-                new LoanProduct(3, "c", 3)
-            };
-
-            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
-
             List<MonthlyRepaymentComparison> comparisons =
                 sut.CompareMonthlyRepayments(new LoanTerm(30));
 
@@ -47,15 +68,6 @@ namespace Loans.Tests
         [Test]
         public void ReturnComparisonForFirstProduct()
         {
-            var products = new List<LoanProduct>
-            {
-                new LoanProduct(1, "a", 1),
-                new LoanProduct(2, "b", 2),
-                new LoanProduct(3, "c", 3)
-            };
-
-            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
-
             List<MonthlyRepaymentComparison> comparisons =
                 sut.CompareMonthlyRepayments(new LoanTerm(30));
 
@@ -68,25 +80,8 @@ namespace Loans.Tests
         [Test]
         public void ReturnComparisonForFirstProduct_WithPartialKnownExpectedValues()
         {
-            var products = new List<LoanProduct>
-            {
-                new LoanProduct(1, "a", 1),
-                new LoanProduct(2, "b", 2),
-                new LoanProduct(3, "c", 3)
-            };
-
-            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
-
             List<MonthlyRepaymentComparison> comparisons =
                 sut.CompareMonthlyRepayments(new LoanTerm(30));
-
-            // Don't care about the expected monthly repayment, only that the product is there
-            //Assert.That(comparisons, Has.Exactly(1)
-            //                             .Property("ProductName").EqualTo("a")
-            //                             .And
-            //                             .Property("InterestRate").EqualTo(1)
-            //                             .And
-            //                             .Property("MonthlyRepayment").GreaterThan(0));
 
             Assert.That(comparisons, Has.Exactly(1)
                                         .Matches<MonthlyRepaymentComparison>(
